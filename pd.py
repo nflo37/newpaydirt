ef7bbeb36d2cefb70f1e28b9b5ac221ba0929ddf
Nick <nflo37@gmail.com>
2025-03-16 12:47:22 -0500

    game starts

--- pd.py
+++ pd.py
@@ -39,7 +39,7 @@ class Team:
         self.teamsheet = Playsheet(f"{self.name}.yaml")
         self.score = 0
         self.timeouts = 3
-        self.possesion = False
+        #self.possesion = False
         self.selected_play = ""
 
 class Game:
@@ -56,6 +56,7 @@ class Game:
     KICKOFF_RETURN_PLAYS = ["Kickoff Return"]
     OFFENSE_PLAYS = ["Line Plunge", "Off Tackle", "End Run", "Draw", "Screen",
                       "Short Pass", "Medium Pass", "Long", "Sideline"]
+    SP_OFFENSE_PLAYS = ["Field Goal", "Punt"]
     DEFENSE_PLAYS  = ["Standard", "Nickel", "Dime", "Prevent", "Blitz"]
 
     def __init__(self):
@@ -64,14 +65,16 @@ class Game:
         self.down = 0 
         self.distance = 0
         self.quarter = 1
-        self.seconds = 15*6  #Quarter is 15 min long. 10 second increments
+        self.seconds = 15*60  #Quarter is 15 min long. 10 second increments
         self.game_over = False
         self.user_team = False
         self.comp_team = False #TODO should i show these here even tho they get set later?
-        self.play_phase = False
+        self.play_state = False
+        self.possession = False
+
 
         #TODO game has a direction it is being played in
-        self.direction = "left"   #Game starts moving right to left
+        self.direction = "right"   #Game starts moving left to right
 
     
     def run_game(self):
@@ -88,6 +91,7 @@ class Game:
 
         evaluate_play_phase:
             Determine result of both team play selections
+            Display the play outcome
 
         post_play_phase:
             Update game state based on play resolution
@@ -98,8 +102,8 @@ class Game:
 
         while not self.game_over:
             self.pre_play_phase()
-            self.evaluate_play_phase()
-            self.post_play_phase() #TODO pickup here, update game state
+            result = self.evaluate_play_phase()  #Net yards from play
+            self.post_play_phase(result)
 
     def start_phase(self, user_team=False, comp_team=False):
         """
@@ -115,7 +119,7 @@ class Game:
         #User team will just start with ball for now
         #TODO Coin toss
         #TODO Game options like quarter length?
-        self.user_team.possesion = True
+        self.possession = self.user_team
         self.setup_kickoff()
 
     def select_teams(self):
@@ -157,7 +161,7 @@ class Game:
         kickoff placement depends on direction of game. 
         Assuming always going left to right
 
-        Whoever does not have possesion kicks off
+        Whoever does not have possession kicks off
         Ball starts on -15 yard line (35 yd line left to right)
         Down is 0 
 
@@ -207,21 +211,25 @@ class Game:
         num_plays = 0
 
         if self.play_state == "kickoff":
-            plays = Game.KICKOFF_PLAYS
+            if self.possession == self.user_team:
+                plays = Game.KICKOFF_RETURN_PLAYS
+            elif self.possession == self.comp_team
+                plays = Game.KICKOFF_PLAYS
+
             for index,play in enumerate(plays):
                 play_choices = play_choices + f"[{index}]: {play}\n"
                 num_plays = len(plays)
                 comp_play = "Kickoff Return"
 
         elif self.play_state == "offense":
-            plays = GAME.OFFENSE_PLAYS
+            plays = Game.OFFENSE_PLAYS + Game.SP_OFFENSE_PLAYS
             for index,play in enumerate(plays):
                 play_choices = play_choices + f"[{index}]: {play}\n"
                 num_plays = len(plays)
                 comp_play = random.choice(Game.DEFENSE_PLAYS)
 
         elif self.play_state == "defense":
-            plays = GAME.DEFENSE_PLAYS
+            plays = Game.DEFENSE_PLAYS
             for index,play in enumerate(plays):
                 play_choices = play_choices + f"[{index}]: {play}\n"
                 num_plays = len(plays)
@@ -258,22 +266,44 @@ class Game:
         user_play_outcomes = self.get_play_outcomes(self.user_team)
         comp_play_outcomes = self.get_play_outcomes(self.comp_team)
 
-        #TODO check for all non-yardage scenarios 
+        #TODO check for all non-yardage scenarios (need to add these to playsheet) 
+        #Need to handle special roll for if user/comp is on defense
+        if self.play_state == "offense":
+            user_roll = random.choice(list(user_play_outcomes.items()))  #[play, yardage]?
+            comp_roll = random.choice(list(comp_play_outcomes[self.user_team.selected_play].items()))
+        elif self.play_state == "defense":
+            user_roll = random.choice(list(user_play_outcomes[self.comp_team.selected_play].items()))  #[play, yardage]?
+            comp_roll = random.choice(list(comp_play_outcomes.items()))
+        else:
+            user_roll = random.choice(list(user_play_outcomes.items()))  #[play, yardage]?
+            comp_roll = random.choice(list(comp_play_outcomes.items()))
 
-        #print(self.user_team.teamsheet.defense)
-        user_roll = random.choice(list(user_play_outcomes.items())) 
-        comp_roll = random.choice(list(comp_play_outcomes.items()))
-        
-        result, result_string = self.get_play_result(user_roll[1], comp_roll[1])
+        user_roll_num = user_roll[0]
+        comp_roll_num = comp_roll[0]
+        user_result = user_roll[1]
+        comp_result = comp_roll[1]
 
+        result, result_string = self.get_play_result(user_result, comp_result)
         self.display_play_results(user_roll, comp_roll, result_string)
 
-    def get_play_result(self, user_result, comp_result):
-        if self.play_state == "kickoff":
-            result = abs(user_result - comp_result)
-            result_string = f"Kickoff: Net {result} yards"
+        return result
 
+
+    def get_play_result(self, user_result, comp_result):
+        
+        #result = abs(user_result - comp_result)
+
+        if self.play_state == "kickoff":
+            result = user_result - comp_result
+            result_string = f"Kickoff: Net {result} yards"
+        elif self.play_state == "offense":
+            result = user_result + comp_result
+            result_string = f"{self.user_team.name} gained {result} yards"
+        elif self.play_state == "defense":
+            result = user_result + comp_result
+            result_string = f"{self.comp_team.name} gained {result*-1} yards"
         return result, result_string
+
 
     def display_play_results(self, user_roll, comp_roll, result_string):
         """
@@ -315,10 +345,120 @@ class Game:
 
         return play_outcomes
 
+
+    def post_play_phase(self, result):
+        """
+        Update game state 
+            Ball position
+            Check for touchdown
+            Check for firstdown
+            update down and distance accordingly
+            Update clock (TODO allow for timeout first?)
+
+        self.ball_position = 15              #Think 50yd line will map to 0. So for kickoff 35-> (15 or -15)
+        self.down = 0 
+        self.distance = 0
+        self.quarter = 1
+        self.seconds = 15*6  #Quarter is 15 min long. 10 second increments
+        """
+        #TODO what about Special teams plays?
+
+        self.update_ball_position(result)
+
+        if self.check_for_touchdown():
+            #Display TD, Update game clock only 10 seconds, Update score 
+            #TODO Need to indicate we are in post-td (XP or go for 2) game state 
+            self.display_touchdown()
+            self.update_game_clock(10)
+            self.update_game_score(6)
+        elif self.play_state == "kickoff":
+            self.down = 1
+            self.distance = 10 #TODO what about first and goal?
+            self.update_game_clock(10)
+            self.update_game_direction()
+            self.transition_from_kickoff()
+        elif self.check_for_firstdown(result):
+            self.down = 1
+            self.distance = 10 #TODO what about first and goal?
+            self.update_game_clock(40)
+        elif self.check_for_turnover_on_downs():
+            self.down = 1
+            self.distance = 10      #TODO what about first a goal?
+            self.swap_possession()
+            self.update_game_direction()
+            self.update_game_clock(10)
+        else:
+            self.down = self.down + 1
+            self.distance = self.distance - result
+            self.update_game_clock(40)
         
+        #TODO handle end of game/quarter
+
+
+    def transition_from_kickoff(self):
+        if self.play_state == "kickoff" and self.possession == self.user_team:
+            self.play_state = "offense"
+        elif self.play_state == "kickoff" and self.possession == self.comp_team:
+            self.play_state == "defense"
+
+
+    def update_game_direction(self):
+        if self.direction == "right":
+            self.direction = "left"
+        else:
+            self.direction = "right"
+
+    def swap_possession(self):
+        if self.possession == self.user_team:
+            self.possession = self.comp_team
+            self.play_state = "defense"
+        else:
+            self.possession = self.user_team
+            self.play_state = "offense"
+    
+    def update_game_clock(self, time_elapsed):
+        self.seconds = self.seconds - time_elapsed
+    
+    def update_game_score(self, points):
+        self.possesion.score = self.possesion.score + points
+
+    def update_ball_position(self, result):
+        if self.direction == "right":
+            self.ball_position = self.ball_position + result
+        else:
+            self.ball_position = self.ball_position - result
+
+    def check_for_touchdown(self):
+        if self.ball_position <= -50 or self.ball_position >= 50:
+            return True
+        else:
+            return False
+
+    def check_for_firstdown(self, result):
+        if result >= self.distance:
+            return True
+        else:
+            return False
+
+    def check_for_turnover_on_downs(self):
+        #We already know it wasnt a 1st down
+        if self.down == 4:
+            print(colored("Turnover on Downs", "yellow", attrs=["blink"]))
+            return True
+        else:
+            return False
+    
+    def display_touchdown(self):
+        touchdown_message = f'Touchdown {self.possession.name}!!!!!'
+        print(colored(touchdown_message, "cyan"))
+
     def print_game_state(self):
-        print(f'SCORE:    {colored(self.user_team.name, "red", attrs=["bold"])}: {self.user_team.score} - {self.comp_team.name}: {self.comp_team.score}')
+        if self.possession == self.user_team:
+            print(f'SCORE:    {colored(self.user_team.name, "red", attrs=["bold"])}: {self.user_team.score} - {self.comp_team.name}: {self.comp_team.score}')
+        else:
+            print(f'SCORE:    {self.user_team.name}: {self.user_team.score} - {colored(self.comp_team.name, "red", attrs=["bold"])}: {self.comp_team.score}')
         print(f"Timeouts  {colored(self.user_team.name, attrs=['bold'])}: {self.user_team.timeouts}   {self.comp_team.name}: {self.comp_team.timeouts}")
+        print(f"Time Remaining: {self.seconds // 60}:{self.seconds % 60}")
         print(f"{self.down} and {self.distance} on the {self.convert_yardage()} ")
 
     def convert_yardage(self):
@@ -339,8 +479,14 @@ def main():
 
     game = Game()
     game.start_phase("atlanta_falcons", "dallas_cowboys")
-    game.pre_play_phase()
-    game.evaluate_play_phase()
 
+    i = 0
+    while(i<20):
+
+        game.pre_play_phase()
+        result = game.evaluate_play_phase()
+        game.post_play_phase(result)
+
+        i = i + 1
 
 main()
